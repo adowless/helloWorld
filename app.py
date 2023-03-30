f503113570b9c81fa36212a6c99c8984351f0d85
adowless <adowless@terpmail.umd.edu>
2023-03-29 21:35:53 -0400

    Fix Syntax errors for contact.html

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ def about():  # put application's code here
 def favorite_course():  # put application's code here
     return render_template ('favorite-course.html')
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():  # put application's code here
+    if request.method == 'POST':
+        return render_template ('contact.html', form_submitted=True)
+    else:
+        return render_template ('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
